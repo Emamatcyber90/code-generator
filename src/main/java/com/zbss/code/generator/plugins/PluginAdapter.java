d88c4cf61c7cf91c65ee38a2a7b37e98577357dc
zhaoyongbing <476223177@qq.com>
2018-08-16 21:36:11 +0800

    [update]

--- src/main/java/com/zbss/code/generator/plugins/PluginAdapter.java
+++ src/main/java/com/zbss/code/generator/plugins/PluginAdapter.java
@@ -4,37 +4,12 @@ import com.zbss.code.generator.table.TableInfo;
 
 import java.util.List;
 
+/**
+ * @author zbss
+ * @desc
+ * @date 2018/8/16 21:35 
+ */
 public class PluginAdapter extends Plugin {
-
-    @Override
-    public void pluginGenerateJavaModel(List<TableInfo> tableInfoList) {
-
-    }
-
-    @Override
-    public void pluginGenerateJavaMapper(List<TableInfo> tableInfoList) {
-
-    }
-
-    @Override
-    public void pluginGenerateXml(List<TableInfo> tableInfoList) {
-
-    }
-
-    @Override
-    public void pluginGenerateJavaController(List<TableInfo> tableInfoList) {
-
-    }
-
-    @Override
-    public void pluginGenerateJavaService(List<TableInfo> tableInfoList) {
-
-    }
-
-    @Override
-    public void pluginGenerateJavaServiceImpl(List<TableInfo> tableInfoList) {
-
-    }
 
     @Override
     public void pluginJavaModel(List<TableInfo> tableInfoList) {
@@ -63,66 +38,6 @@ public class PluginAdapter extends Plugin {
 
     @Override
     public void pluginJavaServiceImpl(List<TableInfo> tableInfoList) {
-
-    }
-
-    @Override
-    public void pluginMergeJavaModel(List<TableInfo> tableInfoList) {
-
-    }
-
-    @Override
-    public void pluginMergeJavaMapper(List<TableInfo> tableInfoList) {
-
-    }
-
-    @Override
-    public void pluginMergeXml(List<TableInfo> tableInfoList) {
-
-    }
-
-    @Override
-    public void pluginMergeJavaController(List<TableInfo> tableInfoList) {
-
-    }
-
-    @Override
-    public void pluginMergeJavaService(List<TableInfo> tableInfoList) {
-
-    }
-
-    @Override
-    public void pluginMergeJavaServiceImpl(List<TableInfo> tableInfoList) {
-
-    }
-
-    @Override
-    public void pluginWriteJavaModel(List<TableInfo> tableInfoList) {
-
-    }
-
-    @Override
-    public void pluginWriteJavaMapper(List<TableInfo> tableInfoList) {
-
-    }
-
-    @Override
-    public void pluginWriteXml(List<TableInfo> tableInfoList) {
-
-    }
-
-    @Override
-    public void pluginWriteJavaController(List<TableInfo> tableInfoList) {
-
-    }
-
-    @Override
-    public void pluginWriteJavaService(List<TableInfo> tableInfoList) {
-
-    }
-
-    @Override
-    public void pluginWriteJavaServiceImpl(List<TableInfo> tableInfoList) {
 
     }
 }
